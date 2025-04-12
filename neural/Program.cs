f52ab0fa8c0466d0258c62215b76611a3e845156
Mork <marklomochevsky@gmail.com>
2025-04-12 12:49:43 +0300

    init

---
+++ neural/Program.cs
@@ -0,0 +1,28 @@
+﻿using neural;
+
+Neuron[][] neurons =
+{
+    new Neuron[2],
+    new Neuron[2],
+    new Neuron[1]
+};
+double[][] vars = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]];
+int set = 1;
+neurons = Helper.GenerateNeurons(neurons);
+neurons[0][0].Value = 1;
+neurons[0][0].Connections[0].Weight = 0.45;
+neurons[0][0].Connections[1].Weight = 0.78;
+neurons[0][1].Connections[0].Weight = -0.12;
+neurons[0][1].Connections[1].Weight = 0.13;
+neurons[1][0].Connections[0].Weight = 1.5;
+neurons[1][1].Connections[0].Weight = -2.3;
+neurons[0][1].Value = 0;
+
+Helper.StartActivation(neurons);
+double error = (double.Pow(vars[2][2] - neurons[2][0].Value, 2)) / set;
+Console.WriteLine(neurons[2][0].Value);
+Console.WriteLine(Helper.DeltaW(1, neurons[2][0].Value, neurons[1][0].Value));
+
+/*Console.WriteLine(Helper.ActivateNeurons(neurons, neurons[1][0]).Value);
+Console.WriteLine(Helper.ActivateNeurons(neurons, neurons[1][1]).Value);
+Console.WriteLine(Helper.ActivateNeurons(neurons, neurons[2][0]).Value);*/
---
+++ neural/neural.cs
@@ -0,0 +1,116 @@
+namespace neural;
+
+public class Neuron
+{
+    public int Id { get; set; }
+    public int Layer { get; set; }
+    public int SynapsysConunt { get; set; }
+    public double Value { get; set; }
+    
+    public List<Synapsys> Connections = new List<Synapsys>();
+    
+    
+    
+}
+public class Synapsys
+{
+    public int Id { get; set; }
+    
+    public double Weight { get; set; }
+    
+    public Neuron InputNeuron { get; set; } // input
+    
+    public Neuron OutputNeuron { get; set; } // output
+}
+
+static class Helper
+{
+    static int id = 0;
+    public static double LogSigmoid(double x)
+    {
+        if (x < -45.0) return 0.0;
+        else if (x > 45.0) return 1.0;
+        else return 1.0 / (1.0 + Math.Exp(-x));
+    }
+
+    public static double fIN(double OUT)
+    {
+        return (1 - OUT) * OUT;
+    }
+
+    public static double deltaO(double ideal, double actual)
+    {
+        return (ideal - actual) * fIN(actual);
+    }
+
+    public static double Grad(double delta, double value)
+    {
+        return value * delta;
+    }
+
+    public static double DeltaW(double ideal, double actual, double value)
+    {
+        return 0.7 * Grad(deltaO(ideal, actual), value) + 0.3 * 0;
+    }
+    
+    public static Neuron[][] GenerateNeurons(Neuron[][] neurons)
+    {
+        for (int i = 0; i < neurons.Length; i++)
+        {
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                neurons[i][j] = new Neuron() {Id = id, Layer = i};
+                id++;
+                if (neurons.Length-1 > i)
+                {
+                    neurons[i][j].SynapsysConunt = neurons[i+1].Length;
+                }
+            }
+        }
+        for (int i = 0; i < neurons.Length; i++)
+        {
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                //Console.Write($"{neurons[i][j].SynapsysConunt} || {neurons[i][j].Id}\n");
+                for (int k = 0; k < neurons[i][j].SynapsysConunt; k++)
+                {
+                    Synapsys synaps = new Synapsys() {Weight = new Random().NextDouble() * (1 + 1) - 1, InputNeuron = neurons[i][j], OutputNeuron = neurons[i+1][k]};
+                    neurons[i][j].Connections.Add(synaps);
+                }
+            }
+        }
+        return neurons;
+    }
+
+    public static Neuron ActivateNeurons(Neuron[][] neurons, Neuron neuron)
+    {
+        List<Synapsys> synapsysList = new List<Synapsys>();
+        for (int j = 0; j < neurons[neuron.Layer-1].Length; j++)
+        {
+            synapsysList.AddRange(neurons[neuron.Layer - 1][j].Connections);
+            
+        }
+        synapsysList= synapsysList.Where(N => N.OutputNeuron.Id == neuron.Id).ToList();
+        double result = 0;
+        foreach (var synaps in synapsysList)
+        {
+            var temp = synaps.InputNeuron.Value * synaps.Weight;
+            result += temp;
+        }
+        result = LogSigmoid(result);
+        neuron.Value = result;
+        return neuron;
+    }
+
+    public static Neuron[][] StartActivation(Neuron[][] neurons)
+    {
+        for (int i = 1; i < neurons.Length; i++)
+        {
+            for (int j = 0; j < neurons[i].Length; j++)
+            {
+                neurons[i][j] = ActivateNeurons(neurons, neurons[i][j]);
+            }
+        }
+        return neurons;
+    }
+}
